41594fe9882f6fcd25e0b935316a1cedbcc94608
Marek Połom <marekpolom22@gmail.com>
2021-03-15 16:17:34 +0100

    docker prod container

---
+++ html/precache-manifest.33bbd82a78d8c5f89365c8887fd77dd5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfdef16cf4f07badc28fc241384ec0e8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "91fcdc8d283134e48b24",
+    "url": "/static/css/main.5d107f72.chunk.css"
+  },
+  {
+    "revision": "a2a3c5a7d7c92aba5d03",
+    "url": "/static/js/2.5d8397bc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5d8397bc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91fcdc8d283134e48b24",
+    "url": "/static/js/main.76887b2b.chunk.js"
+  },
+  {
+    "revision": "3910f0aed3fbdb90a576",
+    "url": "/static/js/runtime-main.3dcf8796.js"
+  }
+]);
